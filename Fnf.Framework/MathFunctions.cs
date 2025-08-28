3fd124d3b9fc39cbe44cd15669e275aa62b0b0fd
Taim Daboul <taimtaim20072019@gmail.com>
2025-08-28 12:57:49 +0300

    More sory moenu work

---
+++ Fnf.Framework/MathFunctions.cs
@@ -0,0 +1,7 @@
+﻿namespace Fnf.Framework
+{
+    public static class MathFunctions
+    {
+        public static float Lerp(float a, float b, float t) => a + (b - a) * t;
+    }
+}
--- Fnf/Scripts/StoryMenu.cs
+++ Fnf/Scripts/StoryMenu.cs
@@ -11,37 +11,22 @@ namespace Fnf
 {
     public class StoryMenu : Script
     {
-        Text weekScore;
-        Text weekName;
-
-        Dictionary<string, int> bgs = new();
-        List<(int, Size)> storyw = new();
+        Text weekScore, weekName;
+        Animator leftArrow, rightArrow;
 
         List<(int, Size)> difs = new();
 
-        int currentBg;
-
+        WeekOption[] options;
         int currentWeek;
 
-        Animator leftArrow, rightArrow;
+        float scrollLerp;
 
         void Start()
         {
+            // TODO: Fuck this font system. Make a new one bitch.
             Font font = new("Assets/Fonts/vcr");
-            FontAtlas atlas = new(font, 100, 3, 2, 0, FontAtlas.UpperCase + FontAtlas.LowerCase + 
+            FontAtlas atlas = new(font, 100, 3, 2, 0, FontAtlas.UpperCase + FontAtlas.LowerCase +
                 FontAtlas.Numbers + FontAtlas.Ponctuals + FontAtlas.Space);
-
-            bgs.Add("stage", Texture.GenerateFromPath("Assets/MenuBackgrounds/menu_stage.png", out _));
-            currentBg = bgs["stage"];
-
-            storyw.Add((Texture.GenerateFromPath("Assets/StoryWeeks/tutorial.png", out Size size), size));
-
-            for (int i = 0; i < 6; i++)
-            {
-                storyw.Add((Texture.GenerateFromPath($"Assets/StoryWeeks/week{i+1}.png", out Size sizee), sizee));
-            }
-
-            difs.Add((Texture.GenerateFromPath("Assets/Difficulties/normal.png", out Size size3), size3));
 
             weekScore = new Text(atlas);
             weekScore.text = "WEEK SCORE:0";
@@ -49,6 +34,7 @@ namespace Fnf
             weekScore.height = 60;
             weekScore.width = 1000;
             weekScore.fontSize = 37;
+            Anchor.PositionUILocaly(weekScore, AnchorType.TopLeft, new Vector2(15, 0));
 
             weekName = new(atlas);
             weekName.text = "CUTIE";
@@ -57,10 +43,7 @@ namespace Fnf
             weekName.width = 1000;
             weekName.fontSize = 37;
             weekName.color = new Color(178, 178, 178);
-
-            Anchor.PositionUILocaly(weekScore, AnchorType.TopLeft, new Vector2(15, 0));
             Anchor.PositionUILocaly(weekName, AnchorType.TopRight, new Vector2(15, 0));
-
 
             leftArrow = new();
             rightArrow = new();
@@ -75,6 +58,8 @@ namespace Fnf
             leftArrow.play("idle");
             rightArrow.play("idle");
 
+            difs.Add((Texture.GenerateFromPath("Assets/Difficulties/normal.png", out Size size3), size3));
+
             float maxw = 0;
 
             for (int i = 0; i < difs.Count; i++)
@@ -88,18 +73,66 @@ namespace Fnf
 
             leftArrow.localPosition = new(470 - maxw, -165);
             rightArrow.localPosition = new(470 + maxw, -165);
+
+            // Load weeks data
+            WeekOption tutor = new WeekOption()
+            {
+                stageBg = Texture.GenerateFromPath("Assets/MenuBackgrounds/menu_stage.png", out _),
+                optionTex = Texture.GenerateFromPath("Assets/StoryWeeks/tutorial.png", out Size to),
+                optionSize = to
+            };
+
+            WeekOption week1 = new WeekOption()
+            {
+                stageBg = tutor.stageBg,
+                optionTex = Texture.GenerateFromPath("Assets/StoryWeeks/week1.png", out Size w1o),
+                optionSize = w1o
+            };
+
+            options = new WeekOption[6];
+            options[0] = tutor;
+            options[1] = week1;
+            options[2] = tutor;
+            options[3] = tutor;
+            options[4] = tutor;
+            options[5] = tutor;
         }
 
-        void Update ()
+        void Update()
         {
-            //text.localPosition = Input.GetGridMousePosition();
-
             leftArrow.Update();
             rightArrow.Update();
+
+            if (Input.GetAnyKeysDown(Key.W, Key.Up))
+            {
+                currentWeek--;
+                if(currentWeek < 0) 
+                    currentWeek = 0;
+            }
+
+            if(Input.GetAnyKeysDown(Key.S, Key.Down))
+            {
+                currentWeek++;
+                if (currentWeek >= options.Length)
+                    currentWeek = options.Length - 1;
+            }
+
+            scrollLerp = MathFunctions.Lerp(scrollLerp, currentWeek * 148, Time.deltaTime * 10);
         }
 
         void Render()
         {
+            RenderWeeksSprites();
+            RenderDifficulty();
+
+            OpenGL.Color3(0, 0, 0);
+            OpenGL.BeginDrawing(DrawMode.Quads);
+            OpenGL.Vertex2(1, 1);
+            OpenGL.Vertex2(-1, 1);
+            OpenGL.Vertex2(-1, 0);
+            OpenGL.Vertex2(1, 0);
+            OpenGL.EndDrawing();
+
             weekScore.Render();
             weekName.Render();
 
@@ -107,21 +140,19 @@ namespace Fnf
             float bottom = Window.PixelToViewportVertical(Window.GridSize.height / 2f - 60 - 412);
 
             OpenGL.Color3(1, 1, 1);
-            Texture.Use(currentBg);
+            Texture.Use(options[currentWeek].stageBg);
             OpenGL.BeginDrawing(DrawMode.Quads);
             OpenGL.TextureCoord(1, 0);
-            OpenGL.Vertex2( 1, top);
+            OpenGL.Vertex2(1, top);
             OpenGL.TextureCoord(0, 0);
             OpenGL.Vertex2(-1, top);
             OpenGL.TextureCoord(0, 1);
             OpenGL.Vertex2(-1, bottom);
             OpenGL.TextureCoord(1, 1);
-            OpenGL.Vertex2( 1, bottom);
+            OpenGL.Vertex2(1, bottom);
             OpenGL.EndDrawing();
             Texture.Use(OpenGL.NULL);
 
-            RenderWeeksSprites();
-            RenderDifficulty();
         }
 
         void RenderDifficulty()
@@ -157,26 +188,19 @@ namespace Fnf
 
         void RenderWeeksSprites()
         {
-            float yoffset = -165;
+            float yoffset = -165 + scrollLerp;
 
-
-
-            RenderWeekSprite(0, 1);
-            RenderWeekSprite(1, 1);
-            RenderWeekSprite(2, 1);
-            RenderWeekSprite(3, 1);
-
-            void RenderWeekSprite(int week, float alpha)
+            for (int i = 0; i < options.Length; i++)
             {
-                Size size = storyw[week].Item2;
+                Size size = options[i].optionSize;
 
                 float scale = 1.05f;
                 float w = size.width / 2 * scale;
                 float h = size.height / 2 * scale;
 
-                OpenGL.Color4(Color.White, alpha);
+                OpenGL.Color4(Color.White, i == currentWeek ? 1 : 0.8f);
 
-                Texture.Use(storyw[week].Item1);
+                Texture.Use(options[i].optionTex);
                 OpenGL.BeginDrawing(DrawMode.Quads);
                 OpenGL.TextureCoord(1, 0);
                 OpenGL.Pixel2(w, h + yoffset);
@@ -190,7 +214,15 @@ namespace Fnf
                 Texture.Use(OpenGL.NULL);
 
                 yoffset -= 148;
-            }
+            } 
         }
+    }
+
+    class WeekOption
+    {
+        public int stageBg;
+        public int optionTex;
+         
+        public Size optionSize;
     }
 }
